2447bd24d275a7d726d2f3d49706576762b7d6e7
Numan Mahzabin <numanmahzabin1029@gmail.com>
2022-03-29 15:36:53 +0600

    problem added

---
+++ problem04.py
@@ -0,0 +1,14 @@
+import math
+t = int(input())
+for x in range(0, t):
+    n = int(input())
+    divisor = []
+    print('Case ',x + 1,':', sep='',end='')
+    for i in range(1, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            divisor.append(i)
+            divisor.append(n // i)
+    divisor = sorted(divisor)
+    for i in divisor:
+        print(' ', i, sep='',end='')
+    print()
